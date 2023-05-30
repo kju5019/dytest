5e320ffa9585f757f813339149e9ff704769ae91
- <100209625+Hwangkyeonghun@users.noreply.github.com>
2023-05-30 18:40:53 +0900

    5.30

--- thisisjava/src/practice/acac.java
+++ thisisjava/src/practice/acac.java
@@ -1,9 +1,30 @@
 package practice;
 
-import lombok.Data;
-
-@Data
 public class acac {
-	String name;
+	public static void main(String[] args) {
+		
+		int[] arr = new int[3];
+
+		arr[0] = (int) (Math.random() * 6 + 1);
+		arr[1] = (int) (Math.random() * 6 + 1);
+		arr[2] = (int) (Math.random() * 6 + 1);
+		
+		for(int i=0; i<arr.length-1; i++){
+            int temp = 0;
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i]>=arr[j]){
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+		
+		
+		for(int i=0; i<arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+
+	}
 
 }
